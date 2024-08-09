2ebd2015b83b39f83e49727e9821bcd62518af3a
pdax-vincentmatthewbautista <vincent.bautista@pdax.ph>
2024-08-09 23:04:25 +0800

    APP-695 create constants.dart

---
+++ plotline_sample_app/lib/config/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Color constants
+const primary = Color(0xFF79d862);
+const seconday = Color(0xFF09295c);
+const background = Color(0xFFffffff);
+
+// Text
+const mainText = Colors.black;
+const subText = Color(0xFFFEFEFE);
